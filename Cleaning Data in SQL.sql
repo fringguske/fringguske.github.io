ea594f1c9ca739744db93ad44ff1453db3fc381d
fringguske <153741621+fringguske@users.noreply.github.com>
2024-02-03 11:23:53 +0300

    Update Cleaning Data in SQL.sql

--- Cleaning Data in SQL.sql
+++ Cleaning Data in SQL.sql
@@ -1,4 +1,9 @@
---The dataset contained information on housing data in the Nashville, TN area. I used SQL Server to clean the data to make it easier to use. For example, I converted some dates to remove unnecessary timestamps; I populated data for null values; I changed address columns from containing all of the address, city and state into separate columns; I changed a column that had different representations of the same data into consistent usage; I removed duplicate rows; and I deleted unused columns
+--The dataset contained information on housing data in the Nashville, TN area. 
+--I used SQL Server to clean the data to make it easier to use. 
+--For example, I converted some dates to remove unnecessary timestamps; I populated data for null values;
+--I changed address columns from containing all of the address, city and state into separate columns; 
+--I changed a column that had different representations of the same data into consistent usage;
+--I removed duplicate rows; and I deleted unused columns
 
 --Cleaning Data in SQL Queries
 
